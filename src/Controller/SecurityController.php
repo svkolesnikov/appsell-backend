4c65a0aa0c6b501f491e656241985c7db8c56cdd
pakostnik <pakostnik@appsell.dev>
2018-04-05 17:36:46 +0300

    BO: Login/Logout

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/admin/login", name="sonata_admin_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('security/login.html.twig', array(
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/admin/logout", name="sonata_admin_logout")
+     */
+    public function logout()
+    {
+        $this->get('security.token_storage')->setToken(null);
+        $this->get('request')->getSession()->invalidate();
+
+        return $this->redirect('login');
+    }
+}
