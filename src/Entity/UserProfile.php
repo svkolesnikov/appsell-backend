8f421339195b7c8654754296d845eae5af1bdb32
sixdays <sixdays@appsell.dev>
2018-04-11 17:37:00 +0300

    Таблицу переименовал

--- src/Entity/UserProfile.php
+++ src/Entity/UserProfile.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="userdata.user_profile")
+ * @ORM\Table(name="userdata.profile")
  * @ORM\HasLifecycleCallbacks
  */
 class UserProfile
