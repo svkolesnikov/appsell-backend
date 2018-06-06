cc487a380913c9f3288825acaa289704bfdad92c
sixdays <sixdays@appsell.dev>
2018-06-06 14:09:29 +0300

    Доработка для запуска контейнера на серверах

--- config/bundles.php
+++ config/bundles.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    'Symfony\\Bundle\\FrameworkBundle\\FrameworkBundle' => ['all' => true],
+    'Symfony\\Bundle\\TwigBundle\\TwigBundle' => ['all' => true],
+    'Symfony\\Bundle\\SecurityBundle\\SecurityBundle' => ['all' => true],
+    'Nelmio\\CorsBundle\\NelmioCorsBundle' => ['all' => true],
+    'Doctrine\\Bundle\\DoctrineCacheBundle\\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\\Bundle\\DoctrineBundle\\DoctrineBundle' => ['all' => true],
+    'ApiPlatform\\Core\\Bridge\\Symfony\\Bundle\\ApiPlatformBundle' => ['all' => true],
+    'Symfony\\Bundle\\WebProfilerBundle\\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Sensio\\Bundle\\FrameworkExtraBundle\\SensioFrameworkExtraBundle' => ['all' => true],
 ];
