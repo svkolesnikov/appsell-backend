16d8caada71216a2af719378d422712577fee451
sixdays <sixdays@appsell.dev>
2018-06-14 09:51:37 +0300

    Таблица для хранения базовых комиссий

---
+++ src/Entity/Commission.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\CommissionEnum;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="financedata.commission")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Commission
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    protected $type;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $description;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    protected $ctime;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    protected $mtime;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist(): void
+    {
+        $this->ctime = new \DateTime();
+        $this->mtime = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate(): void
+    {
+        $this->mtime = new \DateTime();
+    }
+
+    /**
+     * @return CommissionEnum
+     * @throws \UnexpectedValueException
+     */
+    public function getType(): CommissionEnum
+    {
+        return new CommissionEnum($this->type);
+    }
+
+    public function setType(CommissionEnum $type)
+    {
+        $this->type = $type->getValue();
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getCtime(): \DateTime
+    {
+        return $this->ctime;
+    }
+
+    public function getMtime(): \DateTime
+    {
+        return $this->mtime;
+    }
+}
---
+++ src/Enum/CommissionEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enum;
+
+use MyCLabs\Enum\Enum;
+
+class CommissionEnum extends Enum
+{
+    // Комиссия, взимаемая сервисом от оплаты заказчиком
+    // 100 – комиссия сервиса
+    public const SERVICE = 'service';
+
+    // Комиссия, взимаемая продавцом от награды сотрудника
+    // 100 - комиссия сервиса - комиссия продавца
+    public const SELLER  = 'seller';
+}
