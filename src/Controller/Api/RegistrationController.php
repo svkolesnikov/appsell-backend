3b6991e70e0daabcd3bd577804b656a5f7c56eab
sixdays <sixdays@appsell.dev>
2018-06-20 14:11:07 +0300

    Перенесена регистрация заказчика и продавца

--- src/Controller/Api/RegistrationController.php
+++ src/Controller/Api/RegistrationController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller\Api;
 
+use App\Exception\AuthException;
+use App\Lib\Controller\FormTrait;
+use App\Lib\Enum\NotificationTypeEnum;
+use App\Notification\Producer\SystemProducer;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Swagger\Annotations\BadRequestResponse;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Validator\Constraints;
 
 /**
  * @Route("/registration")
  */
 class RegistrationController
 {
-    public function __construct()
+    use FormTrait;
+
+    /** @var EntityManagerInterface */
+    protected $entityManager;
+
+    /** @var SystemProducer */
+    protected $systemProducer;
+
+    public function __construct(EntityManagerInterface $em, SystemProducer $sp)
     {
+        $this->entityManager = $em;
+        $this->systemProducer = $sp;
     }
 
     /**
@@ -44,10 +64,43 @@ class RegistrationController
      * )
      *
      * @Route("/sellers", methods = { "POST" })
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \App\Exception\FormValidationException
      */
-    public function registerSellerAction(): JsonResponse
+    public function registerSellerAction(Request $request): JsonResponse
     {
+        $form = $this->createFormBuilder()
+            ->setMethod($request->getMethod())
+            ->add('email', Type\TextType::class, ['constraints' => [new Constraints\Email(), new Constraints\NotBlank()]])
+            ->add('phone', Type\TextType::class, ['constraints' => [new Constraints\NotBlank()]])
+            ->getForm();
 
+        $form->handleRequest($request);
+        $this->validateForm($form);
+        $data = $form->getData();
+
+        $user = new Entity\User();
+        $user->setEmail($data['email']);
+
+        $profile = $user->getProfile();
+        $profile->setPhone($data['phone']);
+
+        try {
+            $this->save($profile);
+        } catch (AuthException $ex) {
+            // В данном случае может возникнуть ситуация, что про заявку все забыли
+            // а чувак снова пытается зарегаться - так что, если у нас уже есть
+            // этот пользователь - вышлем уведомление повторно
+        }
+
+        $this->systemProducer->produce(NotificationTypeEnum::NEW_SELLER(), [
+            'subject' => 'Зарегистрировался новый продавец',
+            'email'   => $data['email'],
+            'phone'   => $data['phone']
+        ]);
+
+        return new JsonResponse(null, JsonResponse::HTTP_CREATED);
     }
 
     /**
@@ -77,11 +130,44 @@ class RegistrationController
      *  @BadRequestResponse()
      * )
      *
-     * @Route("/sellers", methods = { "POST" })
+     * @Route("/owners", methods = { "POST" })
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \App\Exception\FormValidationException
      */
-    public function registerOwnerAction(): JsonResponse
+    public function registerOwnerAction(Request $request): JsonResponse
     {
+        $form = $this->createFormBuilder()
+            ->setMethod($request->getMethod())
+            ->add('email', Type\TextType::class, ['constraints' => [new Constraints\Email(), new Constraints\NotBlank()]])
+            ->add('phone', Type\TextType::class, ['constraints' => [new Constraints\NotBlank()]])
+            ->getForm();
 
+        $form->handleRequest($request);
+        $this->validateForm($form);
+        $data = $form->getData();
+
+        $user = new Entity\User();
+        $user->setEmail($data['email']);
+
+        $profile = $user->getProfile();
+        $profile->setPhone($data['phone']);
+
+        try {
+            $this->save($profile);
+        } catch (AuthException $ex) {
+            // В данном случае может возникнуть ситуация, что про заявку все забыли
+            // а чувак снова пытается зарегаться - так что, если у нас уже есть
+            // этот пользователь - вышлем уведомление повторно
+        }
+
+        $this->systemProducer->produce(NotificationTypeEnum::NEW_OWNER(), [
+            'subject' => 'Зарегистрировался новый заказчик',
+            'email'   => $data['email'],
+            'phone'   => $data['phone']
+        ]);
+
+        return new JsonResponse(null, JsonResponse::HTTP_CREATED);
     }
 
     /**
@@ -112,10 +198,27 @@ class RegistrationController
      *  @BadRequestResponse()
      * )
      *
-     * @Route("/sellers", methods = { "POST" })
+     * @Route("/employees", methods = { "POST" })
      */
     public function registerEmployeeAction(): JsonResponse
     {
 
+    }
+
+    /**
+     * @param Entity\UserProfile $profile
+     * @throws AuthException
+     */
+    private function save(Entity\UserProfile $profile): void
+    {
+        try {
+
+            $this->entityManager->persist($profile);
+            $this->entityManager->persist($profile->getUser());
+            $this->entityManager->flush();
+
+        } catch (UniqueConstraintViolationException $ex) {
+            throw new AuthException('Email already exists', $ex);
+        }
     }
 }
