6adf9b047659887cb704b6e4402fa5f8fafec636
sixdays <sixdays@appsell.dev>
2018-07-05 10:33:45 +0300

    Текст исключения не отдается клиенту

--- src/EventSubscriber/KernelExceptionSubscriber.php
+++ src/EventSubscriber/KernelExceptionSubscriber.php
@@ -55,7 +55,7 @@ class KernelExceptionSubscriber implements EventSubscriberInterface
         }
 
         $response   = [
-            'message' => $exception->getMessage(),
+            'message' => null,
             'details' => []
         ];
 
