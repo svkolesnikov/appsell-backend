088e16b84d62e849edd1c941acfc2ed47c4c3f0e
sixdays <sixdays@appsell.dev>
2018-12-12 21:10:27 +0300

    При образениях к solar staff api отключена проверка SSL

--- src/SolarStaff/Transport.php
+++ src/SolarStaff/Transport.php
@@ -33,8 +33,7 @@ class Transport
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch, CURL_SSLVERSION_SSLv2, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         curl_setopt($ch, CURLOPT_URL, $this->url . '/' . $method);
