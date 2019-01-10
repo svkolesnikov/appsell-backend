ae60b04800c0b52b56ddc158e46e59039d25f7ac
pakostnik <pakostnik@appsell.dev>
2019-01-10 16:52:24 +0300

    Подправил деплой с учетом новых каталогов для картинок

--- build/fpm/docker-entry-point.sh
+++ build/fpm/docker-entry-point.sh
@@ -3,4 +3,7 @@
 chown -R www-data:www-data /var/www/app/var
 chmod -R +w /var/www/app/var
 
+chown -R www-data:www-data /var/www/app/public/images
+chmod -R +w /var/www/app/public/images
+
 exec "$@"
