9967b5666c2209d89a82ce0d502bdf853540dfcf
sixdays <andrey@sixdays.ru>
2019-10-15 16:26:01 +0300

    Удалил старую сборку

--- build/fpm/docker-entry-point.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-chown -R www-data:www-data /var/www/app/var
-chmod -R +w /var/www/app/var
-
-chown -R www-data:www-data /var/www/app/public/images
-chmod -R +w /var/www/app/public/images
-
-exec "$@"
